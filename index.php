78a02ffec61aa2ebc2059dad609f8f013a445bbe
Valentin Patrigot <fnanops@gmail.com>
2020-01-15 15:04:16 +0100

    first commit 2

--- index.php
+++ index.php
@@ -7,16 +7,18 @@
     <link rel="stylesheet" type="text/css" href="css/styleEffacement.css">
     <link rel="icon" type="image/png" href="img/dark_logo.png" />
   </head>
-  <body>
+<body>
     <header>
-      <a href="index.php" id="homeBtn">
+      <a href="index.php" id="homeBtn" data-target="0">
         <h1 id="websiteTitle">
           T-14 Hyperdrive Generator
         </h1>
       </a> 
+    </header>
+    <div id="main">
       <nav>
-        <ul>
-          <a href="index.php?id=tuto" data-target="0">
+        <ul class="nav">
+          <a href="index.php?id=tuto" data-target="-1">
             <li <?php if(!empty($_GET)){ if($_GET["id"] == "tuto") echo "class='btnMenuActive'"; } ?>>
               Tutoriels
             </li>
@@ -43,36 +45,59 @@
           </a>
         </ul>
       </nav>
-    </header>
-    <main>
-      <section class="wrapper" id="main">
-        <?php
-          
-          if(empty($_GET)){
-            include("templates/home.html");
-          }
+      <div class="sceneElement"       
+      <?php 
+      if(!empty($_GET)) {
+        switch ($_GET["id"]) {
+          case 'tuto':
+            echo " data-viewport='-1'";
+            break;
+          case 'liens':
+            echo " data-viewport='1'";
+            break;
+          case 'boutons':
+            echo " data-viewport='2'";
+            break;
+          case 'menus':
+            echo " data-viewport='3'";
+            break;
+          case 'about':
+            echo " data-viewport='4'";
+            break;
+        }
+      }
+    ?>>
+        <main id="main">
+          <section class="wrapper">
+            <?php
+              
+              if(empty($_GET)){
+                include("templates/home.html");
+              }
 
-          else{
+              else{
 
-            $renduPrisEnCharge=array("menus", "boutons", "liens");
-            if(in_array($_GET["id"], $renduPrisEnCharge)){
-              include("templates/page.php");
-            }
-            elseif($_GET["id"] == "about"){
-              include("templates/about.php");
-            }
-            elseif ($_GET["id"] == "tuto") {
-              include("templates/tuto.html");
-            }
-          }
-         ?>
-      </section>
-    </main>
+                $renduPrisEnCharge=array("menus", "boutons", "liens");
+                if(in_array($_GET["id"], $renduPrisEnCharge)){
+                  include("templates/page.php");
+                }
+                elseif($_GET["id"] == "about"){
+                  include("templates/about.php");
+                }
+              }
+             ?>
+          </section>
+        </main>
+      </div>
+    </div>
+
+    <!-- Scripts -->
+    <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
+    <script src="js/jquery.smoothState.min.js"></script>
+    <script src="js/main.js"></script>
+
     <footer>
       &copy IUT Annecy 2020
     </footer>
-    <script src="https://code.jquery.com/jquery-2.2.1.min.js"></script>
-    <script type="text/javascript" src="js/jquery.smoothState.min.js"></script>
-    <script type="text/javascript" src="js/main.js"></script>
   </body>
 </html>
